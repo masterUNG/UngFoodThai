f1a77857a0bd8860c274500b95aea303b8cf24f2
masterUNG <phrombutr@gmail.com>
2019-07-15 14:03:55 +0700

    Design UI Authen Success

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -9,6 +9,76 @@ class _AuthenState extends State<Authen> {
   // Explicit
 
   // Method
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.pinkAccent[200],
+      child: Text(
+        'Sign In',
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      child: Text(
+        'Sign Up',
+        style: TextStyle(color: Colors.pinkAccent[700]),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: signInButton(),
+          ),
+          mySizeBox(),
+          Expanded(
+            child: signUpButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget userText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'User :',
+          labelStyle: TextStyle(color: Colors.pinkAccent[700]),
+          hintText: 'Not Blank',
+        ),
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Password :',
+          labelStyle: TextStyle(color: Colors.pinkAccent[700]),
+          hintText: 'Not Blank',
+        ),
+      ),
+    );
+  }
+
+  Widget mySizeBox() {
+    return SizedBox(
+      height: 16.0,
+      width: 8.0,
+    );
+  }
 
   Widget showLogo() {
     return Container(
@@ -22,9 +92,10 @@ class _AuthenState extends State<Authen> {
     return Text(
       'Ung FoodThai',
       style: TextStyle(
-        fontSize: 36.0,
+        fontSize: 48.0,
         fontWeight: FontWeight.bold,
-        color: Colors.pinkAccent[700],fontFamily: 'DancingScript',
+        color: Colors.pinkAccent[700],
+        fontFamily: 'DancingScript',
       ),
     );
   }
@@ -32,13 +103,25 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [ Colors.purple[300], Colors.purple[50]],
+            begin: Alignment.topCenter,
+          ),
+        ),
         padding: EdgeInsets.only(top: 60.0),
         alignment: Alignment.topCenter,
         child: Column(
           children: <Widget>[
             showLogo(),
+            mySizeBox(),
             showAppName(),
+            userText(),
+            passwordText(),
+            mySizeBox(),
+            showButton(),
           ],
         ),
       ),
